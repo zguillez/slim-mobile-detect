9a9ca2f64ec9619504ff95817c98e6edc2cf3e64
Guillermo de la Iglesia <zguillez@gmail.com>
2016-03-08 18:48:13 +0100

    Fix class files

--- src/Http/MobileResponse.php
+++ src/Http/MobileResponse.php
@@ -3,36 +3,7 @@
 	namespace Slim\Http;
 
 	use Psr\Http\Message\ResponseInterface;
-	use Psr\Http\Message\ServerRequestInterface;
 
-	class MobileRequest extends Request implements ServerRequestInterface
-	{
-		protected $detect;
-		function __construct($request)
-		{
-			parent::__construct($request->originalMethod, $request->uri, $request->headers, $request->cookies, $request->serverParams, $request->body, $request->uploadedFiles);
-			$this->attributes    = $request->attributes;
-			$this->uploadedFiles = $request->uploadedFiles;
-			$this->bodyParsed    = $request->getParsedBody();
-			$this->detect        = new \Mobile_Detect;
-		}
-		public function isMobile()
-		{
-			return $this->detect->isMobile();
-		}
-		public function isTablet()
-		{
-			return $this->detect->isTablet();
-		}
-		public function isiOS()
-		{
-			return $this->detect->isiOS();
-		}
-		public function isAndroidOS()
-		{
-			return $this->detect->isAndroidOS();
-		}
-	}
 	class MobileResponse extends Response implements ResponseInterface
 	{
 		protected $detect;
